77c93a25de23e412d890729fdf434673e3f2b2a3
Atul more <atulmor199@gmail.com>
2026-05-08 14:18:35 +0530

    Q2_celsius_fahrenheit.py

---
+++ Q2_celsius_fahrenheit.py
@@ -0,0 +1,4 @@
+# Q2,Write a program that will convert celsiue valus to fahrenheit
+temp = float(input("enter temperature in celcius"))
+fahrenheit = (temp * 1.8) + 32
+print(f"{temp} C = {fahrenheit:.2f} F")
